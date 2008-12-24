3e217f18895ebf6deaccdc32f4801517a02e6c33
Brian Bommarito <brian@doomydoom.net>
2008-12-24 06:55:21 -0800

    Adding the beginning of the default settings to the project. First one is the main reason I broke my development cycle to add the plugin in the first place: email activation.

---
+++ config/initializers/settings.rb
@@ -0,0 +1,5 @@
+# The following are the defaults for the settings plugin.
+# These are used if a record for the setting does not already exist in the
+# database.
+
+Settings.defaults[:email_activation] = true
