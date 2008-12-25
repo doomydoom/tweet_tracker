99235562bf1d981d60355af0bc9f693b161ab41c
Brian Bommarito <brian@doomydoom.net>
2008-12-25 11:36:39 -0800

    Rewriting the StringExtensionsTest to use only Test::Unit.

--- test/unit/string_extensions_test.rb
+++ test/unit/string_extensions_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StringExtensionsTest < Test::Unit::TestCase
-  should "Clean the string by stripping whitespace, removing whitespace in the string, and downcasing it" do
+  def test_should_clean_the_string
     test_string = " Some Thing Here "
     assert_equal("somethinghere", test_string.clean)
   end
